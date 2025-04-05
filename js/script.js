dd342ba945b7602faf5cae1a8bbe2f325dfae953
romikkm11 <romikkm11@gmail.com>
2025-04-05 22:09:46 +0300

    edit some files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-fetch('http://3.71.86.119/prices')
-  .then(response => response.json())
-  .then(data => {
-    const content = document.getElementById('content');
-    content.innerHTML = data.items ? data.items.map(item => `<p>${item.name}</p>`).join('') : 'No data';
-  })
-  .catch(error => console.error('Error:', error));
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('map').setView([49.8397, 24.0297], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        zoomControl: false,
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    map.zoomControl.remove();
+});
